e490ccd2dfb1b335fa2984e9202c25351ab7f633
bharath-709457 <79187084+bharath-709457@users.noreply.github.com>
2025-01-25 16:22:47 -0600

    Binary_Search of an array

---
+++ java/ClosestToZero/src/Binary_Search.java
@@ -0,0 +1,36 @@
+public class Binary_Search {
+
+    public static void main(String[] args) {
+        int[] arr={-9,-7,-4,-3,-1,1,4,5,7,8,22,87,99};
+        int target=-7;
+        System.out.println(binary_Search(arr,target));
+    }
+
+    public static int binary_Search(int[] arr, int target) {
+        int start=0;
+        int end= (arr.length)-1;
+        boolean isAsc = arr[start] < arr[end];
+        while (start <= end){
+            int mid = start + (end-start)/2;
+
+            if(target == arr[mid]){
+                return mid;
+            }
+            if(isAsc){
+                if(target < arr[mid]){
+                    end = mid-1;
+                }else{
+                    start = mid+1;
+                }
+            }else{
+                if(target > arr[mid]){
+                    end = mid-1;
+                }else{
+                    start = mid+1;
+                }
+            }
+
+        }
+        return -1;
+    }
+}
