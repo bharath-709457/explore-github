7dc3479109589d633315468a0fe481297a33e809
bharath-709457 <79187084+bharath-709457@users.noreply.github.com>
2025-01-29 15:31:29 -0600

    celing a Number using Binary search

--- java/ClosestToZero/src/celing_Number.java
+++ java/ClosestToZero/src/celing_Number.java
@@ -1,7 +1,7 @@
 public class celing_Number {
     public static void main(String[] args) {
         int[] arr={-9,-7,-4,-3,-1,1,4,5,7,8,22,87,99};
-        int target=-5;
+        int target=200;
         System.out.println(ceelingNumber(arr,target));
     }
 
@@ -9,18 +9,22 @@ public class celing_Number {
         int start=0;
         int end=(nums.length)-1;
 
+        if (target > nums[nums.length-1]) {
+            return -1;
+        }
+
         while (start <= end){
             int mid = start+(end-start)/2;
             if(nums[mid] == target){
-                return nums[mid];
+                return mid;
             }
             if (target < nums[mid]) {
-                end = end-1;
+                end = mid-1;
             }else{
                 start=mid+1;
             }
         }
 
-        return nums[start];
+        return start;
     }
 }
