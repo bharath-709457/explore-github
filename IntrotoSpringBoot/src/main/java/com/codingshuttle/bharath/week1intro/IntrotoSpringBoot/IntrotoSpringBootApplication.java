728f2587f55050e6248e60a23154fcc9bce5feec
bharath-709457 <79187084+bharath-709457@users.noreply.github.com>
2025-03-02 09:41:35 -0600

    1.1 setting up project

---
+++ IntrotoSpringBoot/src/main/java/com/codingshuttle/bharath/week1intro/IntrotoSpringBoot/IntrotoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.codingshuttle.bharath.week1intro.IntrotoSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntrotoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntrotoSpringBootApplication.class, args);
+	}
+
+}
---
+++ IntrotoSpringBoot/src/test/java/com/codingshuttle/bharath/week1intro/IntrotoSpringBoot/IntrotoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingshuttle.bharath.week1intro.IntrotoSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntrotoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
